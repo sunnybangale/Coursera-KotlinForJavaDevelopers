c214c63e305e01f48ea24bac7c512250713af10d
Sunny Bangale <sunnybangale@gmail.com>
2019-08-09 10:14:43 -0700

    Added comments and removed unused imports

--- Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -1,13 +1,29 @@
+/**
+ * This code is written for the programming assignment of Coursera assignment.
+ * It has functions to validate if a particular guess made by a user is correct based on the original string.
+ * The game compares two strings and tells how many letters are on the right position and wrong positions.
+ * The player has to iteratively guess the exact string based on the intermediate statistics of the position of colors.
+ *
+ * It is written in Kotlin.
+ *
+ * @author Sunny Bangale
+ *
+ */
 package mastermind
 
 import mastermind.Evaluation
 
+/**
+* This class is to define the evaluation parameters for the secret and the guessed string
+*/
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
+/**
+* This function evaluates a guess based on the secret string and the guessed string by the player.
+* It returns an iterative evaluation of the right and wrong positions of certain color codes in the string.
+*/
 fun evaluateGuess(secret: String, guess: String): Evaluation {
-    println("Secret is $secret")
-    println("Guess is $guess")
-
+    
     var secretMap : MutableMap<Char, HashSet<Int>> = mutableMapOf()
     var countMap : MutableMap<Char, Int> = mutableMapOf()
 
@@ -24,9 +40,6 @@ fun evaluateGuess(secret: String, guess: String): Evaluation {
         }
     }
 
-    //println("Secret Map is $secretMap")
-    //println("Count Map is $countMap")
-
     var rightPosition = 0
     var commonColor = 0
 
@@ -40,11 +53,8 @@ fun evaluateGuess(secret: String, guess: String): Evaluation {
             countMap[guess[i]] = countMap[guess[i]]!! - 1
         }
     }
-
     var wrongPosition = commonColor - rightPosition
 
-    //println("RP $rightPosition")
-    //println("WP $wrongPosition")
     return Evaluation(rightPosition, wrongPosition)
 
 }
